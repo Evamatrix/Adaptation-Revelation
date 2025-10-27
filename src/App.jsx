774ba52b640ef6d903d696bc7ed7b55f13e8cc9a
Evelyn Tran <evelyntran224@gmail.com>
2025-10-27 16:00:39 -0500

    User sign up and verified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './components/SignUp';
+import UserVerified from './components/UserVerified';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <SignUp />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/verified" element={<UserVerified />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
