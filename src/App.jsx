57c74a31f16494a460e11d8a462b84b46db42bec
Alejandra De Alba <alejandradealba@Alejandras-MacBook-Air.local>
2025-10-29 12:44:04 -0500

    Added Route to User Profile. Need to merge or copy files to main.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import UserVerified from './components/UserVerified';
+import UserProfile from './components/UserProfile';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<SignUp />} />
           <Route path="/verified" element={<UserVerified />} />
+          <Route path="/profile" element={<UserProfile />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
